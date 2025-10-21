e4ef853e508da3918bcd95ef9c495c5639d45ee8
ichinose4 <daichan.5270@gmail.com>
2025-10-21 19:10:10 +0900

    initialcommit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by 一瀬大輝 on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
