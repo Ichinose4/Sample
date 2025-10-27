efec6d7b09d6540210ab63dcf6e3ef775f2a2762
ichinose4 <daichan.5270@gmail.com>
2025-10-27 16:14:47 +0900

    chapter5 end

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -5,14 +5,10 @@
 //  Created by 一瀬大輝 on 2025/10/21.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack (spacing:20){
-            Text("こんにちは！")
-            
-            Text("私は山田太郎です")
-        }
-    }
+var body: some View {
+    Button(
+        action:
+            label:{Text("カウント")}
+    )
 }
+
